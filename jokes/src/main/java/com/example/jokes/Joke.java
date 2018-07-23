34906f21972b9be7248db8dd25f69e37b251ecf6
aditishevale <aditi.shevale999@gmail.com>
2018-07-23 14:53:30 +0530

    1st Commit

--- jokes/src/main/java/com/example/jokes/Joke.java
+++ jokes/src/main/java/com/example/jokes/Joke.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class Joke {
 
     public static String jokes() {
+
+
         return "CAPS LOCK – Preventing Login Since 1980. ";
     }
 
-    // public String joke = "FunnyJoke !!";
 
 
 }
